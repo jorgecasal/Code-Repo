51937f151bbf263a6498b5486097ac4ef5da2c78
Jorge Casal <casaldelacruz@gmail.com>
2021-02-25 23:21:19 +0100

    london

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'CODE REPO',
+  shortName: 'BLOG',
+  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'JORGE CASAL',
+  twitter: 'Imed_Adel'
+}
