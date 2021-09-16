b45d278dc180ca49d72c413918665714d5c2de0d
jorgecasal <casaldelacruz@gmail.com>
2021-09-16 10:39:25 +0200

    date link

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: "TypeScript REPO",
+  name: "CODE REPO",
   shortName: "BLOG",
-  description: "A bold, minimal TypeScript tutorial",
-  url: "https://TypeScript-repo.netlify.app/",
+  description: "A bold, minimal JavaScript tutorial",
+  url: "https://code-repo.netlify.app/",
   prefix: "/",
   author: "JORGE CASAL",
   twitter: "jorgecasal",
